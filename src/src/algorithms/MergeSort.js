b110476cb88f8fa1f08e4ce46f42ca5a0f5ef831
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-01-29 15:56:56 -0500

    MergeSort function is working properly.

--- src/src/algorithms/MergeSort.js
+++ src/src/algorithms/MergeSort.js
@@ -1,14 +1,41 @@
 export const mergesort = (array) => {
-    console.log("Array starts like this: ", array)
     let left = array.slice(0, Math.ceil(array.length / 2));
     let right = array.slice(Math.ceil(array.length / 2), array.length+1);
-    console.log("left is: ", left, "Right is: ", right);
     
     //base case is needed to finishing the recursive looping
-    // if () {
-
-    // }
+    if (array.length < 2) {
+        return array;
+    }
     //else statement will recursively call MergeSort until we reach the base case.
-    return "ok";
+    left = mergesort(left);
+    right = mergesort(right);
+    
+        // console.log(array);
+    return merge(left, right);
+    
 }
 
+const merge = (left, right) => {
+    let chars = left.length+right.length;
+    // console.log(chars, left, right);
+    let newarr = [];
+    let j = 0;
+    let k = 0;
+    for (let i = 0; i < chars; i++) {
+        if (j >= left.length) {
+            newarr[i] = right[k];
+            k += 1;
+        } else if (k >= right.length) {
+            newarr[i] = left[j];
+            j += 1;
+        } else if (left[j] <= right[k]) {
+            newarr[i] = left[j];
+            j += 1;
+        } else {
+            newarr[i] = right[k];
+            k += 1;
+        }
+
+    }
+    return newarr;
+}
